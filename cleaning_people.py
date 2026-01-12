4c05d6ce7a7f00f6f71d55f36736ca26170076ac
bhooomyy <bhattbhoomi0111@gmail.com>
2026-01-12 14:58:15 -0500

    Clean people dataset by removing records with missing primary names
    - Loaded IMDb people.tsv dataset with proper null handling for '\N' values
    - Identified records with missing primaryName
    - Removed rows where primaryName is null to ensure data usability
    - Prepared cleaned people dataset for further validation and analysis

---
+++ cleaning_people.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+
+############# cleaning people.tsv ##############
+people_data=pd.read_csv('/Users/bhoomi/Documents/IMDBdataset/people.tsv',sep='\t',na_values=["\\N", ""])
+#print(people_data.head())
+#print(people_data.isna().sum())
+'''
+# primary name missing check
+cnt=0
+for index,row in people_data.iterrows():
+    if pd.isna(row['primaryName']):
+        cnt+=1
+        print(row['nconst'])
+print(cnt)
+'''
+# drop rows with missing primary name
+people_data=people_data.dropna(subset=['primaryName'])
