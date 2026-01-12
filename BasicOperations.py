a373ad3157a34abf49f8e47f903486df607fa7fe
bhooomyy <bhattbhoomi0111@gmail.com>
2026-01-12 15:36:27 -0500

    Add analysis for top-rated titles with minimum vote threshold
    - Filtered title ratings to include titles with at least 50,000 votes
    - Ranked qualifying titles by averageRating in descending order
    - Joined rating data with title metadata to retrieve title names and types
    - Displayed the top 20 highest-rated titles meeting the vote threshold

--- BasicOperations.py
+++ BasicOperations.py
@@ -1,5 +1,5 @@
 from cleaning import people_data,title_alternates_data,title_crew_data,title_episode_data,title_principals_data,title_ratings_data,titles_data
-
+import pandas as pd
 ####### Analysis #########
 #List the first 50 movies released between 2010 and 2015
 titleType_movies=titles_data[titles_data['titleType']=='movie']
@@ -13,3 +13,11 @@ print(titleType_movies_2010_2015[['tconst','primaryTitle','startYear']].head(50)
 cnt_title_titleType=titles_data.groupby(['titleType']).size().reset_index(name='cnt_title_titleType')
 cnt_title_titleType=cnt_title_titleType.sort_values(by='cnt_title_titleType',ascending=False)
 print(cnt_title_titleType)
+
+
+
+#Find the top 20 highest-rated titles that have received at least 50,000 votes.
+tconst_50000_votes=title_ratings_data[title_ratings_data['numVotes']>=50000].sort_values(by='averageRating',ascending=False)
+#print(tconst_50000_votes)
+top_20_movies=pd.merge(titles_data[['tconst','titleType','primaryTitle']],tconst_50000_votes[['tconst','averageRating','numVotes']],on='tconst',how='inner')
+print(top_20_movies.head(20))
