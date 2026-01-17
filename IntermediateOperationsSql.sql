1d35d629637c62a2c596b9cb6db98afb1fca46d4
bhooomyy <bhattbhoomi0111@gmail.com>
2026-01-17 13:42:29 -0500

    Add query to identify titles with the highest number of alternate regional/language titles
    Add query to identify titles with the highest number of alternate regional/language titles

--- IntermediateOperationsSql.sql
+++ IntermediateOperationsSql.sql
@@ -8,3 +8,10 @@ from titles t join title_rating r on t.tconst = r.tconst where t.startYear is no
 select titleType,decade,tconst,primaryTitle,averageRating,numVotes from ranked_titles where rn <= 10 order by titleType, decade, rn;
 
 
+-- Find the top 100 movies or TV series that are available under the highest number of different regional or language-specific titles.
+select  t.tconst,t.primaryTitle,t.titleType,count(distinct ta.title) as num_alt_titles,count(distinct ta.region) as num_regions,count(distinct ta.language) as num_languages
+from titles t join title_alternate ta on ta.titleId = t.tconst 
+where t.titleType in ('movie','tvSeries') 
+group by t.tconst, t.primaryTitle, t.titleType 
+order by num_alt_titles desc, num_regions desc, num_languages desc 
+limit 100;
