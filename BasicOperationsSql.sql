482678453de1e2e2cfa09ce3af2efc85a3fe6d7f
bhooomyy <bhattbhoomi0111@gmail.com>
2026-01-17 13:06:28 -0500

    Add aggregation to count titles by title type
    Added a grouped aggregation query to calculate the total number of titles per title type and order the results by volume for catalog distribution analysis.

--- BasicOperationsSql.sql
+++ BasicOperationsSql.sql
@@ -1,3 +1,6 @@
 use imdb ;
 -- List the first 50 movies released between 2010 and 2015
 select * from titles where titleType='movie' and (startYear>=2010 and startYear<=2015);
+
+-- Count how many titles exist for each title type and display the results in descending order.
+select titleType,count(*) as cnt from titles group by titleType order by cnt desc;
