e034f55afa4e74c95978c77677fcbc0e95d2f607
bhooomyy <bhattbhoomi0111@gmail.com>
2026-01-17 13:06:59 -0500

    Add query to identify highest-rated titles with significant vote counts
    Implemented a query to retrieve the top 20 highest-rated titles that have received at least 50,000 votes, ensuring ranking reliability based on audience engagement.

--- BasicOperationsSql.sql
+++ BasicOperationsSql.sql
@@ -4,3 +4,6 @@ select * from titles where titleType='movie' and (startYear>=2010 and startYear<
 
 -- Count how many titles exist for each title type and display the results in descending order.
 select titleType,count(*) as cnt from titles group by titleType order by cnt desc;
+
+-- Find the top 20 highest-rated titles that have received at least 50,000 votes.
+select t.primaryTitle,t.titleType,r.averageRating,r.numVotes from titles t left join title_rating r on t.tconst=r.tconst where r.numVotes>=50000 order by r.averageRating desc limit 20;
